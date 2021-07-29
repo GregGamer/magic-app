54f4548b4694620464ed41f62782c83a2cac07d2
GregGamer <gregamer98@gmail.com>
2021-07-30 01:39:51 +0200

    changing the search

--- resources/views/archives/search.blade.php
+++ resources/views/archives/search.blade.php
@@ -1,19 +1,13 @@
-<div>
-    <label for="price" class="block text-sm font-medium text-gray-700">Search</label>
-    <div class="mt-1 relative rounded-md shadow-sm">
-        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-      <span class="text-gray-500 sm:text-sm">
-        Sol Ring
-      </span>
-        </div>
-        <input type="text" name="price" id="price" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" placeholder="0.00">
-        <div class="absolute inset-y-0 right-0 flex items-center">
-            <label for="currency" class="sr-only">Currency</label>
-            <select id="currency" name="currency" class="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
-                <option>USD</option>
-                <option>CAD</option>
-                <option>EUR</option>
-            </select>
-        </div>
-    </div>
+<div class=" bg-gray-200">
+  <div class="container h-screen flex justify-center items-center">
+      <div class="relative flex">
+          <div class="top-4 left-3"> 
+            <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i> 
+          </div> 
+          <input type="text" class="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none" placeholder="Search anything...">
+          <div class="top-2 right-2 ml-3"> 
+            <button class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600">Search</button> 
+          </div>
+      </div>
+  </div>
 </div>
