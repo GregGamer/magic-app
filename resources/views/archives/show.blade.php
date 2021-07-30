6bc1428668a3d64882247607bd494a0deefa27c3
GregGamer <gregamer98@gmail.com>
2021-07-30 18:00:36 +0200

    changing views card search

--- resources/views/archives/show.blade.php
+++ resources/views/archives/show.blade.php
@@ -11,9 +11,11 @@
             </a>
         </div>
     </x-slot>
+    
+    @include('archives.search')
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                @include('archives.search')
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @include('archives.cards')
             </div>
