80666b83fab10e43175e5ed4594b6af81903272f
GregGamer <gregamer98@gmail.com>
2021-08-10 00:25:19 +0200

    adding a api fetcher class for api interaction

--- app/Models/FetchScryfallApi.php
+++ app/Models/FetchScryfallApi.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 
 class FetchScryfallApi extends Model
 {
     use HasFactory;
+
+    public static function fetch_RandomCard(){
+        return Http::get("https://api.scryfall.com/cards/random")->object();
+    }
 }
