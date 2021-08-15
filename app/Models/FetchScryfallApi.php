9531a34690d7f47777a3bff81a36aa4d5c495857
GregGamer <gregamer98@gmail.com>
2021-08-16 00:35:53 +0200

    adding bunch of helper methods for editions

--- app/Models/FetchScryfallApi.php
+++ app/Models/FetchScryfallApi.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\EditionController;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
@@ -11,6 +12,9 @@ use App\Http\Controllers\RawCardController;
 class FetchScryfallApi extends Model
 {
     use HasFactory;
+
+    // TODO: Sort out the Code put it away from this class
+    // TODO: Rewrite the printings count logic, since it does not check if it exists
 
     public static function fetch_RandomCard(){
         return Http::get("https://api.scryfall.com/cards/random")->object();
@@ -55,7 +59,48 @@ class FetchScryfallApi extends Model
         return RawCard::where('oracle_id', $oracle_id)->get();
     }
 
-    public static function get_EditionSvg_By_Set(){
+    //////////////////////////////////////////////////////
+    // Edition
+    //////////////////////////////////////////////////////
 
+    public static function fetch_Editions(){
+        return collect(Http::get('https://api.scryfall.com/sets')->object()->data);
     }
+
+    public static function fetch_Edition_By_Set($set_code){
+        return collect(Http::get('http://api.scryfall.com/sets/'.$set_code)->object());
+    }
+
+    public static function store_Editions(){
+        $editions = FetchScryfallApi::fetch_Editions();
+
+        foreach($editions as $edition){
+            FetchScryfallApi::store_Edition_by_EditionObject($edition);
+        }
+    }
+
+    public static function store_Edition_by_EditionObject($edition){
+        if (! Edition::where('code', $edition->code)->exists()){
+            // TODO: unbedingt eine andere Methode fürs speichern verwenden
+            (new EditionController)->store($edition);
+       }
+    }
+
+    public static function check_ApiEditions_with_DbEditions(){
+        return Edition::all()->count() == FetchScryfallApi::fetch_Editions()->count();
+    }
+
+    public static function update_Editions(){
+        $editions = FetchScryfallApi::fetch_Editions();
+        if(! FetchScryfallApi::check_ApiEditions_with_DbEditions()){
+            FetchScryfallApi::store_Editions();
+        }
+        return true;
+    }
+
+    public static function get_EditionSvg_By_Set($set_code){
+        return Edition::where('set', $set_code)->first()->icon_svg_uri;
+    }
+
+
 }
