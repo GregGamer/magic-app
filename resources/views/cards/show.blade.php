cc5a31ce22d9e2013ad37ab4d42aab1ac52b5599
GregGamer <gregamer98@gmail.com>
2021-08-22 16:02:54 +0200

    adding search to single page

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -9,6 +9,7 @@
             </h2>
         </div>
     </x-slot>
+    @include('archives.search')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
