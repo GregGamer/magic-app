f2f40cef89ed6f88f1497661a92050f5e9651625
derGreg <gregor.wagner@sz-ybbs.ac.at>
2021-09-05 23:29:43 +0200

    Fixed the livewire issue, its worse now

--- app/Http/Livewire/CardPrintingCounter.php
+++ app/Http/Livewire/CardPrintingCounter.php
@@ -9,21 +9,23 @@ class CardPrintingCounter extends Component
 {
     public $printing;
     public $archive;
+    //TODO: FIX why is cards->count() not up to date with what is in the database after calling addOne/subOne
+    public $helper_counter;
 
     public function addOne()
     {
         Card::store_Card_By_CardPrinting_And_Archive($this->printing, $this->archive);
-        $this->mount();
+        $this->helper_counter = $this->printing->cards->count()+1;
     }
 
     public function subOne()
     {
         Card::delete_Card_By_CardPrinting_And_Archive( $this->printing, $this->archive);
-        $this->mount();
+        $this->helper_counter = $this->printing->cards->count()-1;
     }
 
     public function render()
     {
-        return view('livewire.card-printing-counter');
+        return view('livewire.card-printing-counter', ['printings_count' => $this->helper_counter]);
     }
 }
--- resources/views/livewire/card-printing-counter.blade.php
+++ resources/views/livewire/card-printing-counter.blade.php
@@ -1,7 +1,7 @@
 <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium flex flex-row space-x-5">
     <button wire:click="subOne">-</button>
 
-    <div class="text-sm text-gray-900">{{ $printing->cards->count() }}</div>
+    <div class="text-sm text-gray-900">{{ $printings_count }}</div>
 
     <button wire:click="addOne">+</button>
 </td>
