7f673b92b04288407d9027e6d848f2a9e0db3cbc
GregGamer <gregamer98@gmail.com>
2021-09-08 14:11:47 +0200

    adding the oracle text

--- resources/views/cards/data.blade.php
+++ resources/views/cards/data.blade.php
@@ -6,9 +6,11 @@
                     <div id="card-data-row">{{$card->name}}</div>
                     <div id="card-data-row">
                         <div id="card-data-col">
-                            <img href="{{json_decode($card->image_uris)->normal}}" alt="Card Printing"/>
+                            <img src="{{json_decode($card->image_uris)->normal}}" alt="Card Printing"/>
                         </div>
-                        <div id="card-data-col"></div>
+                        <div id="card-data-col">
+                            <p>{{ $card->printed_text ? $card->printed_text : $card->oracle_text }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
