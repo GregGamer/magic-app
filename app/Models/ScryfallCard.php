f7fc13ab9e56bd472f39b0fd6079fba315c5f72e
Gregor <gregamer98@gmail.com>
2021-09-21 21:23:20 +0200

    just some changes

--- app/Models/ScryfallCard.php
+++ app/Models/ScryfallCard.php
@@ -7,13 +7,69 @@ use App\Models\Scryfall;
 
 class ScryfallCard extends Scryfall
 {
+    public $raw;
+
+    public function __construct($uuid)
+    {
+       $this->uuid = $uuid;
+       $this->raw = $this->raw();
+    }
+
     public function raw()
     {
+        echo 'http request got sent';
         return Http::get(self::$API_URL . '/cards/' . $this->uuid)->object();
     }
 
     public function edition()
     {
-        return 'edition';
+        return collect(['code' => $this->raw->set, 'name' => $this->raw->set_name]);
     }
+
+    public function artist()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function faces()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function face()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function mana()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function meta()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function printings()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function printing()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function rulings()
+    {
+        return 'WIP: not working yet';
+    }
+
+    public function ruling()
+    {
+        return 'WIP: not working yet';
+    }
+
+
 }
--- app/Models/ScryfallEdition.php
+++ app/Models/ScryfallEdition.php
@@ -7,6 +7,11 @@ use App\Models\Scryfall;
 
 class ScryfallEdition extends Scryfall
 {
+    public static function find($uuid)
+    {
+        return Http::get(self::$API_URL . '/sets/' . $uuid)->object();
+    }
+
     public function raw()
     {
         return Http::get(self::$API_URL . '/sets/' . $this->uuid)->object();
