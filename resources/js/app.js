af2aec11bb51366547984d1ade0483c975c18202
Gregor <gregamer98@gmail.com>
2021-09-30 00:24:44 +0200

    Formating the Document

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,27 +7,27 @@ window.Alpine = Alpine;
 Alpine.start();
 
 $('#magic-search').select2({
-  placeholder: "Search for cards",
-  minimumInputLength: 4,
+    placeholder: "Search for cards",
+    minimumInputLength: 4,
     ajax: {
-      url: 'https://api.scryfall.com/cards/search',
-      dataType: 'json',
-      type: 'GET',
-      data: function (params) {
-        var queryParameters = {
-            q: 'name:' + params.term
-        };
-        return queryParameters;
-      },
-      processResults: function (data) {
-          return {
-              results: $.map(data.data, function (item) {
-                  return {
-                      text: item.name,
-                      id: item.id
-                  };
-              })
-          };
-      },
+        url: 'https://api.scryfall.com/cards/search',
+        dataType: 'json',
+        type: 'GET',
+        data: function (params) {
+            var queryParameters = {
+                q: 'name:' + params.term
+            };
+            return queryParameters;
+        },
+        processResults: function (data) {
+            return {
+                results: $.map(data.data, function (item) {
+                    return {
+                        text: item.name,
+                        id: item.id
+                    };
+                })
+            };
+        },
     }
-    });
+});
