657f45cc56af197c997a681831358681272655b1
Gregor <gregamer98@gmail.com>
2021-09-30 00:24:17 +0200

    Fixing the Search Bug
    fixing the bug and getting rid of the german lanuage this will be set in an other feature as a setting option for the user

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,19 +10,18 @@ $('#magic-search').select2({
   placeholder: "Search for cards",
   minimumInputLength: 4,
     ajax: {
-      url: '/api/cards/fetch',
+      url: 'https://api.scryfall.com/cards/search',
       dataType: 'json',
       type: 'GET',
       data: function (params) {
-
         var queryParameters = {
-            term: params.term
+            q: 'name:' + params.term
         };
         return queryParameters;
       },
       processResults: function (data) {
           return {
-              results: $.map(data, function (item) {
+              results: $.map(data.data, function (item) {
                   return {
                       text: item.name,
                       id: item.id
