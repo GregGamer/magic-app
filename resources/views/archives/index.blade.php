e99c1127f037a60a48edb11b6e299c7dc1833b6b
Gregor <gregamer98@gmail.com>
2021-10-01 23:58:54 +0200

    adding new column to archives and altering the index and the create form plus the controller

--- resources/views/archives/index.blade.php
+++ resources/views/archives/index.blade.php
@@ -13,7 +13,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                @include('archives.table')
+                @include('archives.table', ['archives' => $archives])
+            </div>
+        </div>
+    </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h2>Public Archives</h2>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                @include('archives.table', ['archives' => $public_archives])
             </div>
         </div>
     </div>
